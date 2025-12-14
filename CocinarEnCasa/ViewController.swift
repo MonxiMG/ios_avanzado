33b9e06678518e9ed39e487e998cd55042abcb45
MonseMG <monsemgarcia@gmail.com>
2025-12-14 11:17:24 +0100

    Initial Commit

---
+++ CocinarEnCasa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CocinarEnCasa
+//
+//  Created by Monse Garcia on 14/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
